3ff3c026ba781b54459e2d147698f19719b29e72
siddharth6758 <Sidd6758harth@gmail.com>
2024-12-13 00:32:40 +0530

    FIX/ADD: Manage Books, Add books, Update Books

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from books.views import *
 
 urlpatterns = [
-    path('book-details/<int:id>',BookView.as_view(),name='view-book'),
+    path('book-details/<int:id>',BookView.as_view(http_method_names=['get']),name='view-book'),
     path('manage-books',manage_books,name='manage-book'),
-    path('book-add',BookView.as_view(),name='add-book'),
-    path('book-update/<int:id>',BookView.as_view(),name='update-book'),
+    path('add-books',add_books,name='add-book-page'),
+    path('book-add',BookView.as_view(http_method_names=['post']),name='add-book'),
+    path('book-update/<int:id>',BookUpdateView.as_view(http_method_names=['post']),name='update-book'),
     path('book-delete/<int:id>',BookView.as_view(),name='delete-book')
 ]
--- books/views.py
+++ books/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from books.models import Books
 from library.models import LibraryManagement
 from django.utils.timezone import now
+from django.contrib import messages
 
 class BookView(View):
 
@@ -22,9 +23,55 @@ class BookView(View):
         }
         return render(request,'book_detail.html',context)
 
+    def post(self, request):
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        cover_image = request.FILES.get('cover_image')
+
+        try:
+            Books.objects.create(
+                title=title,
+                author=author,
+                description=description,
+                cover_image=cover_image
+            )
+            messages.success(request,f'Successfully Created!')
+        except Exception as e:
+            print(f'Exception occured during Creating Book | error: {str(e)}')
+            messages.error(request,f'Error while creating: {str(e)}!')
+        return redirect('manage-book')
+
+class BookUpdateView(View):
+
+    def post(self, request, id):
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        description = request.POST.get('description')
+        cover_image = request.FILES.get('cover_image')
+
+        try:
+            book = Books.objects.get(id=id)
+            book.title=title
+            book.description=description
+            book.author=author
+            if cover_image:
+                book.cover_image=cover_image
+            book.save()
+            messages.success(request,f'Successfully Updated!')
+        except Exception as e:
+            print(f'Exception occured during Updating Book: {Books.object.get(id=id).title}, Error: {str(e)}')
+            messages.error(request,f'Error while updating: {str(e)}!')
+        return redirect(f'/book-details/{id}')
+
+
 def manage_books(request):
     books = Books.objects.all()
     context = {
         'books':books
     }
     return render(request,'book_manage.html',context=context)
+
+
+def add_books(request):
+    return render(request,'book_add.html')
